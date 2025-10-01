1dc2594b8b4a80e2d8d118e03e99a248d3d5dff2
motohamy1 <eltohamym660@gmail.com>
2025-10-01 17:12:34 +0300

    fix infinite loop of load resume

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -26,26 +26,26 @@ const Resume = () => {
 
     useEffect(() => {
         const loadResume = async () => {
-            const resume = await kv.get(`resumes:${id}`);
-            if(!resume) return;
+            const resume = await kv.get(`resume:${id}`);
+            if (!resume) return;
 
             const data = JSON.parse(resume);
-            const resumeBlob = await  fs.read(data.resumePath);
-            if(!resumeBlob) return;
+            const resumeBlob = await fs.read(data.resumePath);
+            if (!resumeBlob) return;
 
-            const pdfBlob = new Blob([resumeBlob], {type: 'application/pdf'})
+            const pdfBlob = new Blob([resumeBlob], { type: 'application/pdf' });
             const resumeUrl = URL.createObjectURL(pdfBlob);
             setResumeUrl(resumeUrl);
 
             const imageBlob = await fs.read(data.imagePath);
-            if(!imageBlob) return;
+            if (!imageBlob) return;
             const imageUrl = URL.createObjectURL(imageBlob);
             setImageUrl(imageUrl);
             setFeedback(data.feedback);
-
-            loadResume();
         };
-    }, [id]);
+
+        loadResume();
+    }, [fs, id, kv]);
 
     return (
         <main className='!pt-o'>
